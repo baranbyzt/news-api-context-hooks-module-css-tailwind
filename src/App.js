95211257a72e9f842d22459795842ccba0e4c811
baran beyazıt <byztmain2121@gmail.com>
2022-03-06 23:52:18 +0300

    contex state is finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+
+ import './style/GeneralStyle.css'
+ import React from 'react'
+ import { useContext, useEffect } from 'react'
+import { MainContext } from './contextStore/ContextApi';
 
 function App() {
+const {
+  fetchApple,
+  fetchTesla,
+  fetchUsaNews,
+  fetchTeachCrunch,
+  fetchWalolStreetJournal,
+  apple,
+  tesla,
+  usaNews,
+  teachCrunch,
+  wallStreetJournal} = useContext(MainContext)
+
+useEffect(async () => {
+  await  fetchApple()
+  await  fetchUsaNews()
+  await  fetchTesla()
+  await  fetchTeachCrunch()
+  await  fetchWalolStreetJournal()
+
+
+}, [])
+
+
+console.log(apple[0].author)
+console.log(usaNews[1].author)
+console.log(teachCrunch[0].author)
+console.log(wallStreetJournal[1].author)
+console.log(tesla[4].author)
+
+/*
+console.log(apple[0].author)
+console.log(usaNews[1].author)
+console.log(teachCrunch[0].author)
+console.log(wallStreetJournal[1].author)
+console.log(tesla[4].author)
+*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+
+<p>{`${apple[0]?.author}`}</p>
+<p>{`${usaNews[1]?.author}`}</p>
+<p>{`${teachCrunch[3]?.author}`}</p>
+<p>{`${wallStreetJournal[1]?.author}`}</p>
+<p>{`${tesla[4]?.author}`}</p>
+</>
   );
 }
 
 export default App;
+
+
+/*
+
+
+<p>{`${apple[0]?.author}`}</p>
+<p>{`${usaNews[1]?.author}`}</p>
+<p>{`${teachCrunch[3]?.author}`}</p>
+<p>{`${wallStreetJournal[1]?.author}`}</p>
+<p>{`${tesla[4]?.author}`}</p>
+
+
+*/
